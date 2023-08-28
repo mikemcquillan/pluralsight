664b03b2f5dabb2d5af8aff4f565a02708beb987
mikemcquillan <mike.mcquillan@mcqtech.com>
2023-08-28 13:58:33 +0100

    Add files via upload
    Added SQL Server Stored Procedures course code.

---
+++ Programming SQL Stored Procedures/Module 4/06 Insert Contact Notes Type With Table Update.sql
@@ -0,0 +1,28 @@
+USE Contacts;
+
+DROP PROCEDURE IF EXISTS dbo.InsertContactNotes;
+
+GO
+
+CREATE PROCEDURE dbo.InsertContactNotes
+(
+ @ContactId		INT,
+ @Notes			ContactNote READONLY
+)
+AS
+BEGIN;
+
+DECLARE @TempNotes ContactNote;
+
+INSERT INTO @TempNotes (Note)
+SELECT Note FROM @Notes;
+
+INSERT INTO dbo.ContactNotes (ContactId, Notes)
+	SELECT @ContactId, Note
+		FROM @Notes;
+
+SELECT * FROM dbo.ContactNotes
+	WHERE ContactId = @ContactId
+ORDER BY NoteId DESC;
+
+END;
